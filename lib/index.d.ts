d8803454e0f985840844a46361abdc3dc690841a
djKooks <inylove82@gmail.com>
2018-10-25 18:36:59 +0900

    Fix build errors

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,1 +1,5 @@
-export declare const Greeter: (name: string) => string;
+export interface JsonOption<T1> {
+    exclude?: Array<keyof T1>;
+    makeDate?: Array<keyof T1>;
+}
+export declare function tuned<T1, T2>(obj: T1, option: JsonOption<T1>, instance: T2): T2;
--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ import * as moment from 'moment'
 
 export interface JsonOption<T1> {
   exclude?: Array<keyof T1>
-  makeBool?: Array<keyof T1>
   makeDate?: Array<keyof T1>
 }
 
@@ -18,15 +17,13 @@ export function tuned<T1, T2> (obj: T1, option: JsonOption<T1>, instance: T2): T
 
   for (const key of Object.keys(obj)) {
     const value = obj[key]
+
+    // ignore key-value when key is included in 'exclude' list
     if (option.exclude && option.exclude.indexOf(key as keyof T1) > -1) {
       continue
     }
 
-    if (option.makeBool && option.makeBool.indexOf(key as keyof T1) > -1) {
-      instance[camelize(key)] = Boolean(value)
-      continue
-    }
-
+    // make data as form of 'Date' when key is included in 'exclude' list
     if (option.makeDate && option.makeDate.indexOf(key as keyof T1) > -1) {
       if (moment(value).isValid()) {
         instance[camelize(key)] = moment(value).toDate()
