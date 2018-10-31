2ac0838cb1f04909ad3776c3527f45a32e632e3e
djKooks <inylove82@gmail.com>
2018-10-31 18:54:31 +0900

    Update example

--- example/example.js
+++ example/example.js
@@ -1,12 +1,21 @@
-let tyson = require('../lib/index')
-let dummyJson = {
+let { Tyson, Config } = require('../lib/index')
+
+const dummyUnderscoreJson = {
   "id": 139,
   "group_number": 17,
   "user": "inylove82@gmail.com",
   "phone_number": "000-111-2222",
   "company": "LeapMind",
-  "salary": "Unknown"
+  "salary": "Unknown",
+  "date": "2014-06-01 12:00"
 }
 
 let tunedResult = {}
-console.log(tyson.tuned(dummyJson, {}, tunedResult))
+let cctyson = new Tyson(new Config('camelCase'))
+cctyson.tuned(dummyUnderscoreJson, {makeDate: ['date']}, tunedResult)
+console.log(`camelcase json: ${JSON.stringify(tunedResult)}`)
+
+let tunedResult_sc = {}
+let sctyson = new Tyson(new Config('snake_case'))
+sctyson.tuned(tunedResult, {}, tunedResult_sc)
+console.log(`snake_case json: ${JSON.stringify(tunedResult_sc)}`)
