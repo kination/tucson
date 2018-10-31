ba113c4e06c2dc339fcf99816f3a0cbddf4cf283
djKooks <inylove82@gmail.com>
2018-10-31 18:22:21 +0900

    Update gitignore to ignore build files

--- lib/index.d.ts
+++
@@ -1,5 +0,0 @@
-export interface JsonOption<T1> {
-    exclude?: Array<keyof T1>;
-    makeDate?: Array<keyof T1>;
-}
-export declare function tuned<T1, T2>(obj: T1, option: JsonOption<T1>, instance: T2): T2;
