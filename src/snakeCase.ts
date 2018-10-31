b49fd7ecce7c92912cc867006e130a4f286d6935
djKooks <inylove82@gmail.com>
2018-10-31 18:44:23 +0900

    Remove modules which does not supported TS

---
+++ src/snakeCase.ts
@@ -0,0 +1,13 @@
+export default function snakeCase (target: string): string {
+  const wordSeparators = /[\s\u2000-\u206F\u2E00-\u2E7F\\'!"#$%&()*+,\-.\/:;<=>?@\[\]^_`{|}~]+/
+  const capitals = /[A-Z\u00C0-\u00D6\u00D9-\u00DD]/g
+
+  let str = target.replace(capitals, function(match) {
+    return ' ' + (match.toLowerCase() || match);
+  })
+
+  return str
+    .trim()
+    .split(wordSeparators)
+    .join('_')
+}
